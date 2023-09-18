5fc882f63a8773cb0321d6a30820510f0cd3a591
samani siewe darix <samanidarix@gmail.com>
2023-09-18 15:14:02 +0100

    correct array error in url_image and title

--- pdfdrive/pdfdrive/spiders/pdfdrive_spider.py
+++ pdfdrive/pdfdrive/spiders/pdfdrive_spider.py
@@ -35,11 +35,11 @@ class PdfdriveSpider(scrapy.Spider,):
 
             pdfdrive_item  = PdfdriveItem()
 
-            pdfdrive_item["url_book"] = "http://www.pdfdrive.com" + book.css('a::attr(href)').get(),
+            pdfdrive_item["url_book"] = "http://www.pdfdrive.com" + book.css('a::attr(href)').get()
             pdfdrive_item["size_book"] = book.css("span.fi-size::text").get()
             pdfdrive_item["year"] = int(book.css('span.fi-year::text').get())
             pdfdrive_item["number_pages"] = book.css('span.fi-pagecount::text').get()
-            pdfdrive_item["title"] = book.css('h2::text').get(),
+            pdfdrive_item["title"] = book.css('h2::text').get()
             pdfdrive_item["url_image"] = book.css("img.img-zoom::attr(src)").get()
             pdfdrive_item["langage_book"] = same_book.find_all("span",{"class":"info-green"})[-1].get_text()
             pdfdrive_item["tags"] = [tag.get_text() for tag in same_book.find_all('div', {'class': 'ebook-tags'})[0].find_all('a')]
